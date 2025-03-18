acbe938ef8387d6e304659e218ee442b290e280e
samwanjo41 <hulahoop41@gmail.com>
2025-03-18 23:39:35 +0300

    project initialization

---
+++ src/main/java/com/samwanjo/gateway/MsSpringSecureGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.samwanjo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpringSecureGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpringSecureGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samwanjo/gateway/MsSpringSecureGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samwanjo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSpringSecureGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
